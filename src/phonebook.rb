56ad0c4ab19ff79be9ecad5d706ffd8fc226e9c5
Oreboat <111535650+Oreboat@users.noreply.github.com>
2025-09-17 16:17:41 -0500

    submission

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,31 @@
 class PhoneBook
-    def initialize
-    end
+  def initialize
+    # store entries as a hash: { name => { number: ..., listed: true/false } }
+    @entries = {}
+  end
 
-    def add(name, number, is_listed)
-        raise Exception, "Not implemented"
-    end
+  # Add a new entry
+  def add(name, number, is_listed)
+    @entries[name] = { number: number, listed: is_listed }
+  end
 
-    def lookup(name)
-        raise Exception, "Not implemented"
-    end
+  # Lookup a number by name (only if listed)
+  def lookup(name)
+    entry = @entries[name]
+    return nil if entry.nil? || !entry[:listed]
+    entry[:number]
+  end
 
-    def lookupByNum(number)
-        raise Exception, "Not implemented"
-    end
+  # Lookup a name by number (only if listed)
+  def lookupByNum(number)
+    result = @entries.find { |name, entry| entry[:number] == number && entry[:listed] }
+    result ? result[0] : nil
+  end
 
-    def namesByAc(areacode)
-        raise Exception, "Not implemented"
-    end
+  # Return all listed names with a given area code
+  def namesByAc(areacode)
+    @entries
+      .select { |_, entry| entry[:listed] && entry[:number].start_with?(areacode) }
+      .keys
+  end
 end
--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,37 @@
+# Fibonacci: return the nth Fibonacci number
 def fib(n)
-    raise Exception, "Not Implemented"
+  raise ArgumentError, "n must be >= 0" if n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+  fib(n - 1) + fib(n - 2)
 end
 
+# Palindrome: check if number/string is a palindrome
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+  str = n.to_s
+  str == str.reverse
 end
 
+# nthmax: return the nth largest element in an array
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+  raise ArgumentError, "n out of range" if n >= a.length
+  a.sort.reverse[n]
 end
 
+# freq: return a hash with frequency counts of each element in string
 def freq(s)
-    raise Exception, "Not Implemented"
+  counts = Hash.new(0)
+  s.each_char { |c| counts[c] += 1 }
+  counts
 end
 
+# zipHash: combine two arrays into a hash
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+  raise ArgumentError, "arrays must be same length" if arr1.length != arr2.length
+  Hash[arr1.zip(arr2)]
 end
 
+# hashToArray: convert hash to array of [key, value] pairs
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+  hash.to_a
 end
